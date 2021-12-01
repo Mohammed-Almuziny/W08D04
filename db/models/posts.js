7abd44857ca119c58b891cae5f3fb65b5d9810ef
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-01 14:03:29 +0300

    create posts model

---
+++ db/models/posts.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const posts = new mongoose.Schema({
+  imgUrl: { type: String },
+  desc: { type: String, required: true },
+  createrID: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Users",
+  },
+  timeStamp: { type: Date, default: new Date() },
+});
+
+module.exports = mongoose.model("Posts", posts);
